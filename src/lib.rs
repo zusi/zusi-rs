9ea84b466586b226c9d559ad1f877db3c28d82f1
Jannis Schaefer <j.schaefer@estwx.de>
2020-10-26 11:52:40 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,3 @@ pub(self) use crate::error::{Error, Result};
 mod error;
 pub mod ser;
 
-fn write_u8<W: Write>(writer: &mut W, value: u8) -> Result<()> {
-    let bts = value.to_le_bytes();
-
-    writer.write_all(&bts)?;
-
-    Ok(())
-}
-
-fn read_u8<R: Read>(reader: &mut R) -> Result<()> {
-    unimplemented!()
-}
-
-
-fn write<W, N>(writer: &mut W, value: N) -> Result<()>
-    where W: Write,
-          N: Num {
-    Ok(())
-}
