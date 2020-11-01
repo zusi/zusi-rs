ee03b6bb81a166571958f84ede87f0aecd0bba04
Jannis Schaefer <j.schaefer@estwx.de>
2020-11-01 23:06:17 +0100

    start implementing deserialize trait

--- protocol-derive/src/lib.rs
+++ protocol-derive/src/lib.rs
@@ -1,13 +1,12 @@
-#![allow(unused_variables)]
 extern crate darling;
 extern crate proc_macro;
 
-use darling::ast::{Data, Style};
 use darling::util::SpannedValue;
 use darling::{ast, Error, FromDeriveInput, FromField};
-use proc_macro2::TokenStream;
-use quote::{quote, ToTokens};
-use syn::{parse_macro_input, DeriveInput, Type};
+use syn::{parse_macro_input, DeriveInput};
+
+mod de;
+mod ser;
 
 #[derive(Debug, FromDeriveInput)]
 struct MyTraitReceiver {
@@ -46,7 +45,7 @@ pub fn serialize_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStre
     let receiver = MyTraitReceiver::from_derive_input(&ast).unwrap();
 
     let mut errors: Vec<Error> = Vec::new();
-    let mut gen = impl_serialize(&mut errors, &receiver);
+    let mut gen = ser::impl_serialize(&mut errors, &receiver);
 
     if !errors.is_empty() {
         gen.extend(Error::multiple(errors).write_errors());
@@ -55,110 +54,17 @@ pub fn serialize_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStre
     gen.into()
 }
 
-fn impl_serialize(mut errors: &mut Vec<Error>, input: &MyTraitReceiver) -> TokenStream {
-    if !input.generics.params.is_empty() {
-        errors.push(Error::custom("Can not parse Generic struct"));
-        return TokenStream::new();
+#[proc_macro_derive(Deserialize, attributes(zusi))]
+pub fn deserialize_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let receiver = MyTraitReceiver::from_derive_input(&ast).unwrap();
+
+    let mut errors: Vec<Error> = Vec::new();
+    let mut gen = de::impl_deserialize(&mut errors, &receiver);
+
+    if !errors.is_empty() {
+        gen.extend(Error::multiple(errors).write_errors());
     }
 
-    let name = &input.ident;
-
-    let output = match &input.data {
-        Data::Struct(ds) => impl_serialize_struct(&mut errors, &input.ident, ds),
-        Data::Enum(de) => impl_serialize_enum(&input.ident, de),
-    };
-
-    output
-}
-
-fn impl_serialize_enum(name: &syn::Ident, ds: &Vec<()>) -> TokenStream {
-    unimplemented!()
-}
-
-fn impl_serialize_struct(
-    errors: &mut Vec<Error>,
-    name: &syn::Ident,
-    ds: &ast::Fields<MyFieldReceiver>,
-) -> TokenStream {
-    // errors.err(name,"procastinating instead of doing Bachelor Thesis stuff");
-
-    if ds.style != Style::Struct {
-        errors.push(Error::custom("Fields must be of type Struct"));
-
-        return TokenStream::new();
-    }
-
-    let fields: Vec<_> = ds
-        .fields
-        .iter()
-        .filter_map(|field| {
-            let field_id = &field.id.unwrap_or_default();
-            let field_name = field.ident.as_ref().unwrap();
-            match &field.ty {
-                Type::Path(s) => eprintln!("Hi"),
-                _ => eprintln!("Hoe"),
-            }
-
-            eprintln!("{:?}", field.ty);
-
-            Some(quote! {
-                Serialize::serialize(&self.#field_name, writer, #field_id)?;
-            })
-        })
-        .collect();
-
-    let token_stream2 = quote! {
-        impl Serialize for #name {
-            fn serialize<W: std::io::Write>(&self, writer: &mut W, id: u16) -> std::result::Result<(), std::io::Error> {
-                zusi_protocol::ser::write_node_header(writer, id)?;
-
-                #(#fields)*
-
-                zusi_protocol::ser::write_node_end(writer)?;
-                // let mut #input = 0;
-
-                Ok(())
-            }
-        }
-    };
-
-    token_stream2
-}
-
-impl ToTokens for MyTraitReceiver {
-    fn to_tokens(&self, tokens: &mut TokenStream) {}
-}
-
-#[cfg(test)]
-mod tests {
-    use syn::parse_str;
-
-    use super::*;
-
-    #[test]
-    fn test_struct() {
-        let input = r#"
-#[derive(Serialize)]
-pub struct Foo {
-    #[zusi(id = 0x0001)]
-    bar: bool,
-    baz: i64,
-}"#;
-
-        let parsed = parse_str(input).unwrap();
-        let receiver = MyTraitReceiver::from_derive_input(&parsed).unwrap();
-        let tokens = quote!(#receiver);
-
-        println!(
-            r#"
-INPUT:
-{}
-PARSED AS:
-{:?}
-EMITS:
-{}
-    "#,
-            input, receiver, tokens
-        );
-    }
+    gen.into()
 }
---
+++ protocol-derive/src/ser.rs
@@ -0,0 +1,68 @@
+extern crate darling;
+extern crate proc_macro;
+
+use crate::{MyFieldReceiver, MyTraitReceiver};
+use darling::ast::{Data, Style};
+use darling::{ast, Error};
+use proc_macro2::TokenStream;
+use quote::quote;
+
+pub(crate) fn impl_serialize(mut errors: &mut Vec<Error>, input: &MyTraitReceiver) -> TokenStream {
+    if !input.generics.params.is_empty() {
+        errors.push(Error::custom("Can not parse Generic struct"));
+        return TokenStream::new();
+    }
+
+    let output = match &input.data {
+        Data::Struct(ds) => impl_serialize_struct(&mut errors, &input.ident, ds),
+        Data::Enum(de) => impl_serialize_enum(&input.ident, de),
+    };
+
+    output
+}
+
+fn impl_serialize_enum(_name: &syn::Ident, _ds: &Vec<()>) -> TokenStream {
+    unimplemented!()
+}
+
+fn impl_serialize_struct(
+    errors: &mut Vec<Error>,
+    name: &syn::Ident,
+    ds: &ast::Fields<MyFieldReceiver>,
+) -> TokenStream {
+    if ds.style != Style::Struct {
+        errors.push(Error::custom("Fields must be of type Struct"));
+
+        return TokenStream::new();
+    }
+
+    let fields: Vec<_> = ds
+        .fields
+        .iter()
+        .filter_map(|field| {
+            let field_id = &field.id.unwrap_or_default();
+            let field_name = field.ident.as_ref().unwrap();
+
+            Some(quote! {
+                Serialize::serialize(&self.#field_name, writer, #field_id)?;
+            })
+        })
+        .collect();
+
+    let token_stream2 = quote! {
+        impl Serialize for #name {
+            fn serialize<W: std::io::Write>(&self, writer: &mut W, id: u16) -> std::result::Result<(), std::io::Error> {
+                zusi_protocol::ser::write_node_header(writer, id)?;
+
+                #(#fields)*
+
+                zusi_protocol::ser::write_node_end(writer)?;
+                // let mut #input = 0;
+
+                Ok(())
+            }
+        }
+    };
+
+    token_stream2
+}
