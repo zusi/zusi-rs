555bd6e443fe06c2de77ea56127c69ebba818b4a
Jannis Schaefer <j.schaefer@estwx.de>
2020-11-02 23:22:16 +0100

    tcp_client handshake

--- examples/tcp_client.rs
+++ examples/tcp_client.rs
@@ -1,25 +1,20 @@
 use std::net::TcpStream;
 
+use zusi::verbindungsaufbau::{AckHello, Hello};
 use zusi_protocol::{Deserialize, Serialize};
-use zusi_protocol_derive::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Default, Debug)]
-#[zusi(id = 0x0001)]
-struct HelloMsg {
-    #[zusi(id = 0x0001)]
-    protokoll_version: u16,
-    #[zusi(id = 0x0002)]
-    client_typ: u16,
-    #[zusi(id = 0x0003)]
-    name: String,
-    #[zusi(id = 0x0004)]
-    version: String,
-}
 
 fn main() -> Result<(), std::io::Error> {
-    let mut stream = TcpStream::connect("127.0.0.1:1435")?;
+    let mut stream = TcpStream::connect("127.0.0.1:1436")?;
 
-    let msg: HelloMsg = Deserialize::deserialize_struct(&mut stream).unwrap();
+    let hello = Hello {
+        protokoll_version: 2,
+        client_typ: 2,
+        name: "Fahrpult".to_string(),
+        version: "2.0".to_string(),
+    };
+
+    Serialize::serialize(&hello, &mut stream, 0x0001)?;
+    let msg: AckHello = Deserialize::deserialize_struct(&mut stream)?;
 
     println!("{:?}", msg);
 
