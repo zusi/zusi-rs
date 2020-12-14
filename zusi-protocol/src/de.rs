c5acb93727d277de2b20e869d1fbedb3b314a129
Jannis Schaefer <j.schaefer@estwx.de>
2020-12-14 01:55:15 +0100

    test wrong length for field reported

--- zusi-protocol/src/de.rs
+++ zusi-protocol/src/de.rs
@@ -30,8 +30,12 @@ macro_rules! impl_deserialize_for_num {
                 const SIZE: usize = mem::size_of::<$type>();
 
                 if SIZE != length as usize {
-                    // todo add error type
-                    panic!();
+                    return Err(ProtocolError::Deserialization(format!(
+                        "memory size ({}) of type {} differs from reported length {}",
+                        SIZE,
+                        stringify!($type),
+                        length
+                    )));
                 }
 
                 let mut bts: [u8; SIZE] = [0; SIZE];
@@ -169,7 +173,7 @@ where
         }
     } else {
         return Err(ProtocolError::Deserialization(
-            "calling read_unknown_field is not supported nor useful",
+            "calling read_unknown_field is not supported nor useful".to_string(),
         ));
     }
 
@@ -202,5 +206,13 @@ mod tests {
         let result: u8 = u8::deserialize(&mut &bts[..], 1).unwrap();
 
         assert_eq!(result, 5)
+    }
+
+    #[test]
+    fn u8_wrong_length() {
+        let bts: Vec<u8> = vec![0x05];
+
+        let _result = u8::deserialize(&mut &bts[..], 2)
+            .expect_err("memory size (1) of type u8 differs from reported length 2");
     }
 }
